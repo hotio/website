28a4a8760fad00a977d7473faffa92b99aa179db
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-20 09:30:43 +0200

    live load tags info

--- docs/javascripts/loadJSON.js
+++ docs/javascripts/loadJSON.js
@@ -9,33 +9,50 @@ $(function loadJSON() {
             animateValue(objStars, 0, f.star_count, 1000);
         });
     });
-    $.getJSON('https://raw.githubusercontent.com/' + image + '/master/tags.json', function(data) {
-        $.each(data.tags, function(i, f) {
-            console.log("Tag: " + f.name + ", Version: " + f.version + ", Commit: " + f.commit + ", Last Updated: " + f.lastUpdated);
-            if (f.lastUpdated == "") {
+    $.getJSON('https://api.github.com/repos/' + image + '/branches', function(data) {
+        $.each(data, function(i, f) {
+            var branch = f.name;
+            var commit = f.commit.sha;
+            var commitURL = f.commit.url;
+            if (branch == "master") {
                 return;
             }
             var extraTag = ""
-            if (f.name == "release") {
+            if (branch == "release") {
                 extraTag = " (latest)";
             }
-            var version = ""
-            if (f.version != "") {
-                version = "<code>" + f.version + "</code>";
-            }
-            var d = new Date(f.lastUpdated);
-            var datestring = d.getFullYear() + "-" + (d.getMonth()+1).toString().padStart(2, '0') + "-" + d.getDate().toString().padStart(2, '0') + " " + d.getHours().toString().padStart(2, '0') + ":" + d.getMinutes().toString().padStart(2, '0') + ":" + d.getSeconds().toString().padStart(2, '0');
-            var tblRow = "<tr>" + "<td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/" + image + "/tree/" + f.name + "\">" + f.name + "</a>" + extraTag + "</td>" + "<td>" + f.upstream + "</td>" + "<td>" + version + "</td>" + "<td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/" + image + "/tree/" + f.commit + "\">" + f.commit + "</a></td>" + "<td style=\"white-space:nowrap;\">" + datestring + "</td>" + "</tr>"
-            $(tblRow).appendTo("#tags-table tbody");
-            $("<span>docker pull " + image + ":" + f.name + "</span><br>").appendTo("#tags-codeblock code");
-            $("<span>docker pull " + image + ":" + f.name + "-" + f.version + "</span><br>").appendTo("#tags-codeblock code");
-            $("<span>docker pull " + image + ":" + f.name + "-" + f.commit + "</span><br>").appendTo("#tags-codeblock code");
+            $.getJSON('https://raw.githubusercontent.com/' + image + '/' + f.name + '/VERSION.json', function(data) {
+                var hide = data.hide;
+                if (hide == true) {
+                    return;
+                }
+                var version = data.version;
+                var description = data.description;
+                if (description == undefined) {
+                    description = "";
+                }
+                $.getJSON(commitURL, function(data) {
+                    var commitDate = data.commit.author.date;
+                    var d = new Date(commitDate);
+                    var datestring = d.getFullYear() + "-" + (d.getMonth()+1).toString().padStart(2, '0') + "-" + d.getDate().toString().padStart(2, '0') + " " + d.getHours().toString().padStart(2, '0') + ":" + d.getMinutes().toString().padStart(2, '0') + ":" + d.getSeconds().toString().padStart(2, '0');
+                    if (version == undefined) {
+                        version = d.getUTCFullYear() + (d.getUTCMonth()+1).toString().padStart(2, '0') + d.getUTCDate().toString().padStart(2, '0') + d.getUTCHours().toString().padStart(2, '0') + d.getUTCMinutes().toString().padStart(2, '0') + d.getUTCSeconds().toString().padStart(2, '0');
+                    }
+                    var versionCode = "<code>" + version + "</code>";
+                    console.log("Tag: " + branch + ", Version: " + version + ", Commit: " + commit + ", Last Updated: " + commitDate);
+                    var tblRow = "<tr>" + "<td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/" + image + "/tree/" + branch + "\">" + branch + "</a>" + extraTag + "</td>" + "<td>" + description + "</td>" + "<td>" + versionCode + "</td>" + "<td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/" + image + "/tree/" + commit + "\">" + commit.slice(0,7) + "</a></td>" + "<td style=\"white-space:nowrap;\">" + datestring + "</td>" + "</tr>"
+                    $(tblRow).appendTo("#tags-table tbody");
+                    $("<span>docker pull " + image + ":" + branch + "</span><br>").appendTo("#tags-codeblock code");
+                    $("<span>docker pull " + image + ":" + branch + "-" + version + "</span><br>").appendTo("#tags-codeblock code");
+                    $("<span>docker pull " + image + ":" + branch + "-" + commit.slice(0,7) + "</span><br>").appendTo("#tags-codeblock code");
+                    var codeBlock = document.getElementById("tags-codeblock").getElementsByTagName( 'code' )[0].innerHTML;
+                    if (codeBlock == "") {
+                        document.getElementById("tags-table").remove();
+                        $("<span>Couldn't load tags! Try again later...</span><br>").appendTo("#tags-codeblock code");
+                    }
+                });
+            });
         });
-        var codeBlock = document.getElementById("tags-codeblock").getElementsByTagName( 'code' )[0].innerHTML;
-        if (codeBlock == "") {
-            document.getElementById("tags-table").remove();
-            $("<span>Couldn't load tags! Try again later...</span><br>").appendTo("#tags-codeblock code");
-        }
     });
 });
 
