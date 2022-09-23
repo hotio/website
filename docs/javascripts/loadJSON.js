668e62ad5293fed5b9664ec13c094cb46fdc69e1
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-23 09:10:03 +0200

    tweaks to javascript

--- docs/javascripts/loadJSON.js
+++ docs/javascripts/loadJSON.js
@@ -34,20 +34,25 @@ $(function loadJSON() {
                 $.getJSON('https://cors.hotio.workers.dev/?https://api.github.com/repos/' + image + '/commits/' + commit, function(data) {
                     var commitDate = data.commit.author.date;
                     var commitMessage = data.commit.message;
+                    var commitHTMLUrl = data.html_url;
                     var d = new Date(commitDate);
                     var currentDate = new Date();
                     var days = (d, currentDate) =>{
                         let difference = currentDate.getTime() - d.getTime();
                         let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-                        return TotalDays + " Day(s)";
+                        if (TotalDays ==  1) {
+                            return `${TotalDays} Day`;
+                        } else {
+                            return `${TotalDays} Days`;
+                        }
                     }
                     var datestring = d.getFullYear() + "-" + (d.getMonth()+1).toString().padStart(2, '0') + "-" + d.getDate().toString().padStart(2, '0') + " " + d.getHours().toString().padStart(2, '0') + ":" + d.getMinutes().toString().padStart(2, '0') + ":" + d.getSeconds().toString().padStart(2, '0');
                     if (version == undefined) {
                         version = d.getUTCFullYear() + (d.getUTCMonth()+1).toString().padStart(2, '0') + d.getUTCDate().toString().padStart(2, '0') + d.getUTCHours().toString().padStart(2, '0') + d.getUTCMinutes().toString().padStart(2, '0') + d.getUTCSeconds().toString().padStart(2, '0');
                     }
-                    var versionCode = "<code>" + version + "</code>";
-                    console.log("Tag: " + branch + ", Version: " + version + ", Commit: " + commit + ", Last Updated: " + commitDate + ", Age: " + days(d, currentDate));
-                    var tblRow = "<tr>" + "<td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/" + image + "/tree/" + branch + "\">" + branch + "</a>" + extraTag + "</td>" + "<td>" + description + "</td>" + "<td>" + versionCode + "</td>" + "<td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/" + image + "/commit/" + commit + "\">" + commit.slice(0,7) + "</a></td>" + '<td>' + commitMessage + "</td><td style=\"white-space:nowrap;\">" + datestring + "</td><td>" + days(d, currentDate) + "</td></tr>"
+                    var versionCode = `<code>${version}</code>`;
+                    console.log(`Tag: ${branch}, Version: ${version}, Commit: ${commit}, Last Updated: ${commitDate}, Age: ${days(d, currentDate)}`);
+                    var tblRow = `<tr><td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/${image}/tree/${branch}\">${branch}</a>${extraTag}</td><td>${description}</td><td>${versionCode}</td><td><a class=\"header-icons\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"${commitHTMLUrl}\">${commit.slice(0,7)}</a></td><td>${commitMessage}</td><td style=\"white-space:nowrap;\">${datestring}</td><td>${days(d, currentDate)}</td></tr>`
                     $(tblRow).appendTo("#tags-table tbody");
                 });
             });
