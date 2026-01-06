8a41cccf17e21cba74c1299bbfac0685f81b00f4
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-06 13:13:16 +0100

    update

---
+++ docs/javascripts/tagcopy.js
@@ -0,0 +1,8 @@
+function CopyToClipboard(id) {
+  var r = document.createRange();
+  r.selectNode(document.getElementById(id));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+}
